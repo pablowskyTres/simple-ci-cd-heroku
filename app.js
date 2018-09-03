0961eaa4b5afbbe63789d60acf442ba3157546d0
pablowskyTres <p.badef@gmail.com>
2018-09-03 12:17:59 -0300

    Basic template

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('*', function (req, res) {
+    res.send('Servicio no implementado :(');
+  });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
